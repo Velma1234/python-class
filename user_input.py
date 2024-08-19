99fcf62d82e8b65bac3cfd10c8a8ece4a04cc677
Velma Atieno <atienovelma1@gmail.com>
2024-08-19 12:52:42 +0300

    Added user_input.py with code to take user input and print personalized message

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = (input("enter your Name: "))
+age= int(input("enter your Age: "))
+location = (input("enter your Location: "))
+
+print("Hello my name is %s , I am %d years old, I live in %s" %(name ,age ,location))
